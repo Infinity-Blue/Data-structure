f159a7d478a21f18b35707f13f1f840ae207c022
SUJUNG <39278189+Infinity-Blue@users.noreply.github.com>
2019-03-09 23:01:13 -0500

    Update TrieNode.java

--- Trie/TrieNode.java
+++ Trie/TrieNode.java
@@ -18,7 +18,6 @@ package trie;
  * which will be indexes by the triplet (3,3,3)
  * 
  * 
- * @author Sesh Venugopal
  *
  */
 class Indexes {
@@ -78,7 +77,6 @@ class Indexes {
  * - the first child node
  * - the sibling node
  * 
- * @author Sesh Venugopal
  *
  */
 public class TrieNode {
